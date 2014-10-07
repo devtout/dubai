76903f2d687b2d43b5301c978fff1d8f6c1da68d
João André <joaoandre.ja@gmail.com>
2014-10-07 14:31:14 -0300

    View Index | Template base.html | Template index.html
    - Configuração dos arquivos estáticos(img, js, css)
    - Configuração dos templates HTML
    - Configuração do template base.html para os padrões django
    - Template index.html herdando do template base
    - Ambiente pronto para desenvolvimento das demais soluções

--- dubai/urls.py
+++ dubai/urls.py
@@ -5,13 +5,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dubai.views.home', name='home'),
-    # url(r'^dubai/', include('dubai.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^$', 'dubai_.views.index', name='index'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns += staticfiles_urlpatterns()
--- dubai_/views.py
+++ dubai_/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+# coding: utf-8
+from django.contrib.auth import logout
+from django.template.context import RequestContext
+from django.shortcuts import render_to_response
+
+def index(request):
+     return render_to_response('index.html',{}, context_instance=RequestContext(request))
