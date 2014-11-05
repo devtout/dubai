807bb9130794f3dff99e89e8e7346bcec8cde80c
renanalan <condominiodubai@web1129.kinghost.net>
2014-11-05 19:09:31 -0200

    .

--- dubai/settings.py
+++ dubai/settings.py
@@ -3,7 +3,7 @@ import os
 
 LOCAL = os.path.dirname(os.path.abspath(__file__))
 
-DEBUG = False
+DEBUG = True
 
 ADMINS = (
      ('renan@e-tout.com.br', 'renan@e-tout.com.br'),
@@ -25,7 +25,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['www.condominiodubai.com.br']
+ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
